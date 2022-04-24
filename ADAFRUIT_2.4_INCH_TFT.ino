5e7b059c3cd6d1b4cdf651823bf17049f315164d
irfandigilog <104311041+irfandigilog@users.noreply.github.com>
2022-04-24 19:12:51 +0500

    color Test Code
    This is test code to show different colors on the LCD

---
+++ ADAFRUIT_2.4_INCH_TFT.ino
@@ -0,0 +1,50 @@
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_TFTLCD.h> // Hardware-specific library
+#include <SPI.h>
+
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+#define PIN_SD_CS 10 // Adafruit SD shields and modules: pin 10
+
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+
+// Assign human-readable names to some common 16-bit color values:
+#define  BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println(F("TFT LCD test"));
+
+#ifdef USE_ADAFRUIT_SHIELD_PINOUT
+  Serial.println(F("Using Adafruit 2.4\" TFT Arduino Shield Pinout"));
+#else
+  Serial.println(F("Using Adafruit 2.4\" TFT Breakout Board Pinout"));
+#endif
+
+  Serial.print("TFT size is "); Serial.print(tft.width()); Serial.print("x");
+  Serial.println(tft.height());
+
+  tft.reset();
+  tft.begin(0x9341);
+}
+void loop()
+{
+  tft.fillScreen(BLUE); delay(1000);
+  tft.fillScreen(RED);  delay(1000);
+  tft.fillScreen(GREEN);delay(1000);
+  tft.fillScreen(BLUE);delay(1000);
+  tft.fillScreen(CYAN);delay(1000);  
+  
+    
+    }
